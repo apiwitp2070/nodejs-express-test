6afad4cfc157426c213eee87f5e7fb4cf5ecb3c7
apiwit2070 <zenzoza007@gmail.com>
2022-04-23 20:49:23 +0700

    add template website

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const chalk = require('chalk');
 const debug = require('debug');
 const morgan = require('morgan');
 
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
   res.send('Hello Express');
-})
+});
 
 app.listen(port, () => {
-  debug(`Listening on port ${chalk.green(port)}`)
-})
+  debug(`Listening on port ${chalk.green(port)}`);
+});
